852e9322a176307877c1048497218d5c23a02e4a
vKarthiga <karthigavenkat971@gmail.com>
2024-06-30 18:46:17 +0530

    script.js

--- prodigy_wd_01/script.js
+++ prodigy_wd_01/script.js
@@ -1,1 +1,10 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
 
